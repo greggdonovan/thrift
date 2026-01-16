58595f05f47b195582aefb7dd13240518baa9e91
Gregg Donovan <gdonovan@etsy.com>
2026-01-15 19:54:56 -0500

    py: modernize Tornado server and HTTP TLS setup

--- lib/py/src/server/THttpServer.py
+++ lib/py/src/server/THttpServer.py
@@ -117,10 +117,13 @@ class THttpServer(TServer.TServer):
         self.httpd = server_class(server_address, RequestHander)
 
         if (kwargs.get('cafile') or kwargs.get('cert_file') or kwargs.get('key_file')):
-            context = ssl.create_default_context(cafile=kwargs.get('cafile'))
-            context.check_hostname = False
+            context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            if kwargs.get('cafile'):
+                context.load_verify_locations(cafile=kwargs.get('cafile'))
+                context.verify_mode = ssl.CERT_REQUIRED
+            else:
+                context.verify_mode = ssl.CERT_NONE
             context.load_cert_chain(kwargs.get('cert_file'), kwargs.get('key_file'))
-            context.verify_mode = ssl.CERT_REQUIRED if kwargs.get('cafile') else ssl.CERT_NONE
             self.httpd.socket = context.wrap_socket(self.httpd.socket, server_side=True)
 
     def serve(self):
